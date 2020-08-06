8bd76ec1596468507e9be3261bf520d50c4ce4a7
Janis Rancans <rancans@gmail.com>
2020-08-07 02:36:10 +0300

    Update bulk_create.sh

--- bulk_create.sh
+++ bulk_create.sh
@@ -7,7 +7,7 @@ if [ "$counter" -gt "20" ] || [ -z $filename ] || ! [[ "$counter" =~ ^[0-9]+$ ]]
 then
         echo "Please provide correct args:"
         echo "Number must be smaller than 20"
-        echo "Must have name"i
+        echo "Must have name"
         echo "Example: ./bulk_create.sh 10 name"  
 else
 	for i in $(seq 0 $counter);
