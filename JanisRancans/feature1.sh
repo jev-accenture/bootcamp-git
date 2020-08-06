3121ddf1abff3af0c549d9815ac6555e31841538
Janis Rancans <janis.rancans@accenture.com>
2020-08-06 23:25:28 +0000

    Bash script for Accenture Git Bootcamp

---
+++ JanisRancans/feature1.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+counter=$1
+filename=$2
+
+if [ "$counter" -gt "20" ] || [ -z $filename ] || ! [[ "$counter" =~ ^[0-9]+$ ]];
+then
+        echo "Please provide correct args:"
+        echo "Number must be smaller than 20"
+        echo "Must have name"i
+        echo "Example: ./bulk_create.sh 10 name"  
+else
+	for i in $(seq 0 $counter);
+        do
+		bash_location=$(which bash)
+                echo "#!"$bash_location > $2_$i.sh
+                chmod u+x $2_$i.sh
+        done
+fi
